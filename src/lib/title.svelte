04a34397ffcdab1ef99a1b1937299f88e292310a
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-06-13 21:18:37 +0200

    added title component

---
+++ src/lib/title.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let title = 'Spaceflake Gaming';
+</script>
+
+<div class="title">
+	<img src="/controller.svg" alt="site logo" />
+	<h1>{title}</h1>
+</div>
+
+<style>
+	.title {
+		display: flex;
+	}
+
+	.title img {
+		display: inline-block;
+	}
+
+	.title h1 {
+		margin-left: 10px;
+	}
+</style>
