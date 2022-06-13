d29c4d5bc00eeb698d9d97f7941ae474fb0567fc
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-06-13 21:18:53 +0200

    added layout files

---
+++ src/routes/__layout-reset.svelte
@@ -0,0 +1,55 @@
+<script>
+	// @ts-ignore
+	import Title from '$lib/title.svelte';
+	import '../styles/global.css';
+</script>
+
+<header>
+	<nav>
+		<Title title="Game Guides" />
+		<div class="links">
+			<a href="/">Home</a>
+			<a href="/about">About</a>
+			<a href="/guides">Guides</a>
+		</div>
+	</nav>
+</header>
+<main>
+	<!-- page content -->
+	<slot />
+</main>
+<footer>
+	<p>Copyright 2022 Spaceflake Gaming Guides</p>
+</footer>
+
+<style>
+	header {
+		text-align: center;
+		background-color: rgba(0, 0, 0, 0.1);
+	}
+	main {
+		width: min(100% - 1rem);
+		max-width: 960px;
+		margin: 20px auto;
+		flex-grow: 1;
+	}
+
+	.links {
+		margin-left: auto;
+	}
+	a {
+		margin-left: 10px;
+	}
+	nav {
+		display: flex;
+		align-items: center;
+		width: min(100% - 1rem);
+
+		max-width: 960px;
+		margin: 0 auto;
+	}
+	footer {
+		margin-top: 1rem;
+		text-align: center;
+	}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,31 @@
+<script>
+	// @ts-ignore
+	import Title from '$lib/title.svelte';
+	import '../styles/global.css';
+</script>
+
+<header>
+	<Title title="Spaceflake Gaming Guides" />
+</header>
+<main>
+	<!-- page content -->
+	<slot />
+</main>
+<footer>
+	<p>Copyright 2022 Spaceflake Gaming Guides</p>
+</footer>
+
+<style>
+	header {
+		display: flex;
+		justify-content: center;
+	}
+	main {
+		flex-grow: 1;
+		width: 100%;
+		margin: 20px auto;
+	}
+	footer {
+		text-align: center;
+	}
+</style>
