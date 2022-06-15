928b294e1c2c6bc195edf78b51151c3a7fbd0bbb
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-06-15 09:31:46 +0200

    custom error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,25 @@
+<script lang="ts" context="module">
+	/** @type {import('@sveltejs/kit').Load} */
+	export function load({ error }: any) {
+		return {
+			props: { message: error?.message }
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let message: string;
+</script>
+
+<div class="error">
+	<h2>Ooops</h2>
+	<p>{message}</p>
+	<a href="/guides">View all Guides...</a>
+</div>
+
+<style>
+	.error {
+		text-align: center;
+		margin-bottom: 60px;
+	}
+</style>
